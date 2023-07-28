87153674f29a844cb9008545e93952af7f0d0fc2
DESKTOP-5L1SBPO\Anthony Mancini <mancini5@uwindsor.ca>
2023-07-28 13:21:18 -0400

    Update

--- pages/index.js
+++ pages/index.js
@@ -85,7 +85,7 @@ export default function Home() {
             <h1 className="text-xl font-extralight  text-black dark:text-white">My Portfolio</h1>
             <ul className="flex items-center">
               <li><RiMoonFoggyLine onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-4xl fill-gray-900 dark:fill-white"/></li>
-              <li><a className="bg-gradient-to-r from-green-700 to-green-500  text-white px-4 py-2 rounded-md ml-8"href="./AnthonyManciniResume.pdf">Resume</a></li>
+              <li><a className="bg-gradient-to-r from-green-700 to-green-500  text-white px-4 py-2 rounded-md ml-8"href="./MyResume.pdf">Resume</a></li>
             </ul>
           </nav>
           <div className='text-center p-10 font-mono'>
